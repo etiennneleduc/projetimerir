4530755473746f5a966d2c289030e8fa75e39139
etiennneleduc <67732467+etiennneleduc@users.noreply.github.com>
2020-07-02 11:02:33 +0200

    Update pageweb.php

--- pageweb.php
+++ pageweb.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
-
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-    <meta charset="utf-8" />
-    <title>Alarme velo </title>
-</head>
-<body>
-    <img alt="logo imerir" src="logo_imerir.png" width="200" height="100" />
-
-    <?php
+<html>
+    <head>
+        <title>Alarme velo </title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h2>Page de test</h2>
+        
+        <p>
+                <img alt="logo imerir" src="logo_imerir.png" width="200" height="100" />
+                <p><h3> Etat du vélo </h3></p>
+                <p> variable actif/ nonactif </p>
+                <p><h3> Coordonnées GPS du vélo </h3></p>
+            Cette page contient du code HTML avec des balises PHP.<br />
+            <?php /* Insérer du code PHP ici */ ?>
+            Voici quelques petits tests :
+        </p>
+        
+        <ul>
+        <li style="color: blue;">Altitude : </li>
+        <li style="color: red;">Longitude : </li>
+        <li style="color: green;">Altitude : </li>
+        </ul>
+        
+        <?php
     $ch = curl_init();
     
 // C'est ici que vous mettez les options comme l'url, timeout, returntransfer
@@ -27,18 +41,7 @@
     
     // Fermeture
     curl_close($ch);
-?>
-
-    <p><h3> Etat du vélo </h3></p>
-    <p> variable actif/ nonactif </p>
-    <p><h3> Coordonnées GPS du vélo </h3></p>
-    <p>
-        <ul>
-        $output
-            <li>Altitude : </li>
-            <li>Longitude : variable</li>
-            <li>Altitude : variable</li>
-        </ul>
-    </p>
-</body>
+        ?>
+    </body>
 </html>
+
